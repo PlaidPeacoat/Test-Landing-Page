f1c1859b03f0782c3ea5bee8bc3d76f57d4d0964
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2023-12-28 13:42:53 -0500

    updated team member names

--- src/app/employee-card/employee-card.component.ts
+++ src/app/employee-card/employee-card.component.ts
@@ -14,19 +14,19 @@ export class EmployeeCardComponent {
       imageUrl: 'https://media.licdn.com/dms/image/D4E03AQFujxrNQmZ-mw/profile-displayphoto-shrink_800_800/0/1685901525389?e=1709164800&v=beta&t=1bSe2mu5NVY93ZXH1KOaqWpsi_FsfcDLse78-dJwEb8'
     },
     {
-      name: 'Name',
+      name: 'Marek',
       jobTitle: 'Praktikant Softwareentwicklung',
       bio: ' 1 Jahre Erfahrung in der Entwicklung',
       imageUrl: 'https://pngimg.com/uploads/face/face_PNG5668.png'
     },
     {
-      name: 'Name',
+      name: 'David',
       jobTitle: 'Backend Software Entwickler',
       bio: ' 5 Jahre Erfahrung in der Entwicklung von Backend-Software...',
       imageUrl: 'https://pngimg.com/uploads/face/face_PNG5668.png'
     },
     {
-      name: 'Name',
+      name: 'Anna',
       jobTitle: 'Database Administrator',
       bio: ' Findet die richtigen Daten für die richtigen Leute...',
       imageUrl: 'https://pngimg.com/uploads/face/face_PNG5668.png'
